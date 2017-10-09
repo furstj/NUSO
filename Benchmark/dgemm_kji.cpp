b9f7f73399fb60713cdbdad5bfe64b2ce0261218
Jiri Furst <Jiri.Furst@gmail.com>
2017-10-09 11:17:51 +0200

    Uprava benchmarku pro Julia 0.6 a pridani c++ verze blokoveho nasobeni matic

---
+++ Benchmark/dgemm_kji.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <chrono>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+  //const int lda = 1024;
+  const int lda = 1001;
+  const int n = 1000;
+  
+  double* a = new double[lda*lda];
+  double* b = new double[lda*lda];
+  double* c = new double[lda*lda];
+
+  for (int i=0; i<n; i++)
+    for (int j=0; j<n; j++) {
+      a[i+lda*j] = i+j;
+      b[i+lda*j] = i-j;
+      c[i+lda*j] = 0.0;
+    };
+
+  auto t1 = chrono::system_clock::now();
+  
+  for (int k=0; k<n; k++)
+    for (int j=0; j<n; j++) 
+      for (int i=0; i<n; i++)
+	c[i+lda*j] += a[i+lda*k] * b[k+lda*j];
+    
+
+  auto t2 = chrono::system_clock::now();
+
+  std::chrono::duration<double> elapsed_time = t2 - t1;
+
+  double mflops = 2*pow(double(n),3)/elapsed_time.count()/1000000.0;
+
+  cout << "time   = " << elapsed_time.count() << endl;
+  cout << "mflops = " << mflops << endl;
+
+  return 0;
+}
